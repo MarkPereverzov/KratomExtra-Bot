dd9f7c506bcb1547690d29426d08e25ce4e73e3b
OzzeY72 <nabacap72@gmail.com>
2023-12-06 15:56:10 +0100

    +ReplyKeyboardMarkup

--- Bot.py
+++ Bot.py
@@ -1,10 +1,18 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import CommandHandler, CallbackQueryHandler, ContextTypes, ApplicationBuilder
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup
 import sqlite3
 from config import TOKEN
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(update.effective_chat.id, 'Привет')
+    await context.bot.send_message(update.effective_chat.id, 'Привет',reply_markup=GetReplyKeyboardMarkup())
+
+async def add_lector(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,text='Send lector name')
+    return EXPECT_LECTOR_NAME
+
+def GetReplyKeyboardMarkup():
+    return ReplyKeyboardMarkup([["test1","test2"]])
+
     
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler(["start","hello"], start))
