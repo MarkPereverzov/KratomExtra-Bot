772b07ab798bbacbb3f54ea0a6d9a482a2a0fa3a
OzzeY72 <nabacap72@gmail.com>
2023-12-06 16:10:28 +0100

    migrate reply_markup

--- Bot.py
+++ Bot.py
@@ -1,18 +1,15 @@
-from telegram.ext import CommandHandler, CallbackQueryHandler, ContextTypes, ApplicationBuilder
+from telegram.ext import CommandHandler, CallbackQueryHandler, ContextTypes, ApplicationBuilder,ConversationHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup
 import sqlite3
 from config import TOKEN
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(update.effective_chat.id, 'Привет',reply_markup=GetReplyKeyboardMarkup())
+    reply_markup = ReplyKeyboardMarkup([["test1","test2"]])
+    await context.bot.send_message(update.effective_chat.id, 'Привет',reply_markup=reply_markup)
 
 async def add_lector(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,text='Send lector name')
     return EXPECT_LECTOR_NAME
-
-def GetReplyKeyboardMarkup():
-    return ReplyKeyboardMarkup([["test1","test2"]])
-
     
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler(["start","hello"], start))
