f85a55b92c11384dd74047d0b94ef256b05bd058
OzzeY72 <nabacap72@gmail.com>
2023-12-11 17:41:39 +0100

    Transit classes to stict type and begin imigration on sqlalchemy

--- classes/Orders.py
+++ classes/Orders.py
@@ -1,8 +1,28 @@
-class Orders:
-    def __init__(self,time,userid):
-        self.time = time
-        self.userid = userid
-        self.orderelements = []
-        self.id = 0
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from typing import List
+from classes.User import User
+from classes.OrderElements import OrderElements
+
+class Base(DeclarativeBase):
+    pass
+
+class Orders(Base):
+    __tablename__ = "Orders"
+
+    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    time: Mapped[int] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
+    orderelements: Mapped[List["OrderElements"]] = relationship(backref="Orders", cascade="all, delete-orphan")
+
+    # def __init__(self,time,userid):
+    #     self.time = time
+    #     self.userid = userid
+    #     self.orderelements = []
+    #     self.id = 0
     def __repr__(self) -> str:
         return f"**Час замовлення:** {self.time}"
--- classes/User.py
+++ classes/User.py
@@ -1,14 +1,38 @@
-class User:
-    def __init__(self,userid,name,surname,phone,city,post_type,post_number):
-        self.userid = userid
-        self.name = name
-        self.surname = surname
-        self.phone = phone
-        self.city = city
-        self.post_type = post_type
-        self.post_number = post_number
-        self.orders = []
-        self.id = id
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from typing import List
+from classes.Orders import Orders 
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    userid: Mapped[str] = mapped_column(String(30))
+    name: Mapped[str] = mapped_column(String(30))
+    surname: Mapped[str] = mapped_column(String(30))
+    phone: Mapped[str] = mapped_column(String(30))
+    city: Mapped[str] = mapped_column(String(30))
+    post_type: Mapped[str] = mapped_column(String(30))
+    post_number: Mapped[int] = mapped_column()
+    orders: Mapped[List["Orders"]] = relationship(backref="Users", cascade="all, delete-orphan")
+
+    # def __init__(self,userid,name,surname,phone,city,post_type,post_number):
+    #     self.userid = userid
+    #     self.name = name
+    #     self.surname = surname
+    #     self.phone = phone
+    #     self.city = city
+    #     self.post_type = post_type
+    #     self.post_number = post_number
+    #     self.orders = []
+    #     self.id = 0
 
     def __repr__(self) -> str:
         return f"Ім'я: {self.name}\nПрізвище: {self.surname}\n"+f"Телефон: {self.phone}\nМісто: {self.city}\n"+f"Почтомат/відділення: {self.post_type}\n"+f"Номер почтомату/відділення: {self.post_number}\n"
